9149fcd97fa95afd0c96f6a0cdbe6047a85ab08d
Leonid <lkashin@gmail.com>
2019-08-25 14:24:09 +0400

    все кроме второго задания

---
+++ Lesson05/lesson05.sql
@@ -0,0 +1,40 @@
+-- "Операторы, фильтрация, сортировка и ограничения"
+-- 1.
+UPDATE users SET created_at = NOW();
+UPDATE users SET updated_at = NOW();
+
+-- 2.
+
+
+-- 3.
+INSERT INTO storehouses_products (storehouse_id, product_id, value) VALUES (1, 1, 50), (1, 1, 25), (1, 1, 2), (1, 1, 30), (1, 1, 20), (1, 1, 1
+50), (1, 1, 0);
+SELECT * FROM storehouses_products ORDER BY IF(value > 0,1,0) DESC, value ASC;
+
+-- 4.
+SELECT * FROM users WHERE SUBSTRING(birthday_at, 7, 1) = 5 OR substring(birthday_at, 7, 1) = 8;
+
+-- 5.
+SELECT * FROM catalogs WHERE id IN(5, 1, 2) ORDER BY FIELD(id,'5','1','2');
+
+-- "Агрегация данных"
+-- 1.
+SELECT AVG(2019 - substring(birthday_at, 1, 4)) FROM users;
+
+-- 2.
+SELECT COUNT(*) as total, dayname(CONCAT_WS('-', '2019', substring(birthday_at, 6, 5))) as wd FROM users GROUP BY wd ORDER BY wd;
+
+-- 3.
+DROP TABLE IF EXISTS storehouses_products;
+CREATE TABLE storehouses_products (
+  id SERIAL PRIMARY KEY,
+  storehouse_id INT UNSIGNED,
+  product_id INT UNSIGNED,
+  value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Запасы на складе';
+
+INSERT INTO storehouses_products (storehouse_id, product_id, value) VALUES (1, 1, 1), (1, 1, 2), (1, 1, 3), (1, 1, 4), (1, 1, 5);
+
+SELECT ROUND(EXP(SUM(LOG(value)))) FROM storehouses_products;
