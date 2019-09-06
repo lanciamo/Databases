1d46d00ce1c807268d6557037d72e34d4a78ab0b
Silveron <lkashin@gmail.com>
2019-09-06 14:47:13 +0300

    version 3

--- Lesson06/lesson6.sql
+++ Lesson06/lesson6.sql
@@ -3,9 +3,8 @@
 -- 1. Переанализировал 100 раз, но пока это самое сложное ДЗ) Возможно из-за того, что исходные данные в таблицах все же надо более четко увязывать с заданиями, чтобы хотя бы проверить зарпос можно было)
 
 -- 2.
-SET @a = /* ...  перепробовал очень много своих юзеров, но везде empty set. А как собрать запрос, чтобы перебрать разом всех юзеров, чтобы найти тех, у кого хотя бы не empty set, не понимаю*/
-
-SELECT COUNT(*), from_user_id FROM messages WHERE to_user_id = @a
+SELECT lastname, @a := id, (
+SELECT COUNT(*) FROM messages WHERE to_user_id = @a
 AND from_user_id IN (
   (SELECT friend_id 
   FROM friendship 
@@ -22,6 +21,9 @@ AND from_user_id IN (
   )
 ) GROUP BY from_user_id
 LIMIT 1
+) AS number_messages
+FROM users
+ORDER BY number_messages DESC
 ;
 
 -- 3.
